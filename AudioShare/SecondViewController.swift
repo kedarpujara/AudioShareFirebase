cde6b3e11e8e84aac669b436fa8e225693551158
Kedar <kedarpujara@gmail.com>
2017-06-21 01:34:58 -0700

    Initial Commit

---
+++ AudioShare/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AudioShare
+//
+//  Created by Kedar Pujara on 6/21/17.
+//  Copyright © 2017 kpujara. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
